f33d71c186413e81b2a0fa8a59ebe03163e76828
jvsguerra <jvsguerra@gmail.com>
2023-09-22 18:40:59 -0300

    Update package name

---
+++ AtomPacker/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for AtomPacker."""
+
+__author__ = """João V S Guerra"""
+__email__ = 'jvsguerra@gmail.com'
+__version__ = '0.1.0'
+
+from . import AtomPacker
+from .AtomPacker import packmol, PackmolStructure, CavityDetector, AtomPacker
