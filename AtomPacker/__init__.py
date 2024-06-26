eeea0f4db4e80e254f130a40ecb85ae4ec25ac1e
jvsguerra <jvsguerra@gmail.com>
2024-06-26 16:18:35 -0300

    Update AtomPacker version

--- AtomPacker/__init__.py
+++ AtomPacker/__init__.py
@@ -9,7 +9,7 @@ molecular structures, packing nanoclusters into these cavities, and exporting \
 the resulting structures.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __name__ = "AtomPacker"
 license = "GNU GPL-3.0 License"
 
